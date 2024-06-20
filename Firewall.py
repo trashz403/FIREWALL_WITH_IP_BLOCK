efe1dd4a014e7392033b2c2959068b8a49b555f0
TRASHZ403 <z403trash@proton.me>
2024-06-20 04:19:52 -0500

    Second commit

--- Firewall.py
+++ Firewall.py
@@ -1,21 +1,22 @@
 import os
 import socket
 
-# Ip Checking 
+# IP Checking 
 def check_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(("",0))
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+    s.bind(("", 0))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     s.settimeout(0.2)
     try:
-        message, address=s.recvfrom(8192)
-        return adress[0]
+        # This is typically a placeholder to simulate receiving data
+        message, address = s.recvfrom(8192)
+        return address[0]
     except socket.timeout:
         return None
-    
+
 # Blacklisted IP Addresses
-blocked_ips={}
+blocked_ips = {}
 
 # Blocking IP Addresses
 def block_ip(ip_address):
@@ -25,4 +26,4 @@ def block_ip(ip_address):
 while True:
     ip_address = check_ip()
     if ip_address and ip_address not in blocked_ips:
-        blocked_ip(ip_address)
+        block_ip(ip_address)
